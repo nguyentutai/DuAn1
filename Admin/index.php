9d012456f001b2d567294d9b3067bee127d9b9c1
tai19062004 <nguyentutai1964@gmail.com>
2023-11-22 00:30:29 +0700

    22/11 12h30

--- Admin/index.php
+++ Admin/index.php
@@ -1,22 +1,49 @@
 <?php
 
 include 'view/header.php';
+include '../model/pdo.php';
+include '../model/danhmucsp.php';
+include '../model/taikhoan.php';
 
 if (isset($_GET['act']) && ($_GET['act'] != '')) {
     $act = $_GET['act'];
     switch ($act) {
-
         case 'adddm':
-            
-            include 'QLDM/add.php';
+            if(isset($_POST['btn-submit'])){
+                $error = [];
+                if(empty($_POST['name-category'])){
+                    $error['name-category'] = 'Vui lòng nhập tên danh mục';
+                }else{
+                    $name_category = $_POST['name-category'];
+                }
+                if(empty($_POST['link'])){
+                    $error['link'] = 'Vui lòng điền đường dẫn tĩnh';
+                } else{
+                    $form_control = $_POST['link'];
+                }
+                $filename = $_FILES['image']['name'];
+                $target_dir = "../upload/";
+                $target_file = $target_dir . basename($_FILES['image']['name']);
+                if (!move_uploaded_file($_FILES['image']['tmp_name'], $target_file)) {
+                    $error['image'] = 'Bạn chưa upload ảnh';
+                }
+
+                $parent_category = $_POST['parent-category'];
+                
+                if(empty($error)){
+                    insert_category($filename,$name_category,$form_control,$parent_category);
+                }
+            }
+            $loaddm = load_category();
+            include './QLDM/add.php';
             break;
         case 'listdm':
 
-            include 'QLDM/list.php';
+            include './QLDM/list.php';
             break;
         case 'addsp':
 
-            include 'QLSP/add.php';
+            include './QLSP/add.php';
             break;
         case 'listsp':
 
--- model/danhmucsp.php
+++ model/danhmucsp.php
@@ -1,12 +1,13 @@
 <?php
-    //Load danh mục
-    function load_category(){
-        $sql = "SELECT * FROM `category`";
-        return pdo_query($sql);
-    }
-    //Add danh mục
-    function insert_category(){
-        $sql = "INSERT INTO `category`(`image_category`, `name_category`, `parent_id`) VALUES ('[value-2]','[value-3]','[value-4]')";
-        pdo_execute($sql);
-    }
-?>
+//Load danh mục
+function load_category()
+{
+    $sql = "SELECT * FROM `category`";
+    return pdo_query($sql);
+}
+//Add danh mục
+function insert_category($image_category, $name_category,$link_category, $parent_id)
+{
+    $sql = "INSERT INTO `category`(`image_category`, `name_category`, `link_category`, `parent_id`) VALUES ('$image_category','$name_category','$link_category','$parent_id')";
+    pdo_execute($sql);
+}
